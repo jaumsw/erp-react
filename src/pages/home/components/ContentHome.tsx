54b39d5e24841aab8878ee4b2b958b7421399b6b
Jaum <j.calixtodegodoy@gmail.com>
2024-02-05 18:38:17 -0300

    feat: new menu itens

---
+++ src/pages/home/components/ContentHome.tsx
@@ -0,0 +1,96 @@
+import {
+  ShoppingCartIcon,
+  CheckIcon,
+  DocumentCheckIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState, useContext } from "react";
+import { Button } from "@/components/ui/button";
+import { addDays, format } from "date-fns";
+import { Calendar } from "@/components/ui/calendar";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { DateRange } from "react-day-picker";
+import { AuthContext } from "@/shared/contexts/AuthContext";
+
+const ContentHome = () => {
+  const { user } = useContext(AuthContext);
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: new Date(2024, 0, 20),
+    to: addDays(new Date(2024, 0, 20), 20),
+  });
+
+  return (
+    <div className="ml-6 mt-16 w-full pr-4">
+      <span className="text-xl font-semibold px-2 pt-5">
+        Olá, <span>{user?.fullname}</span>
+      </span>
+      <div className="flex">
+        <span className="text-sm font-semibold text-zinc-500 px-2 pt-3">
+          Um ótimo dia para você!!
+        </span>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              className="bg-blue-600 hover:bg-blue-800 hover:text-white text-white border-none ml-auto"
+              id="date"
+              variant={"outline"}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {date?.from ? (
+                date.to ? (
+                  <>
+                    {format(date.from, "LLL dd, y")} -{" "}
+                    {format(date.to, "LLL dd, y")}
+                  </>
+                ) : (
+                  format(date.from, "LLL dd, y")
+                )
+              ) : (
+                <span>Selecione uma data</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              initialFocus
+              mode="range"
+              defaultMonth={date?.from}
+              selected={date}
+              onSelect={setDate}
+              numberOfMonths={2}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+      <div className="flex w-full mt-10">
+        <div className="w-1/4 h-40 bg-zinc-100 rounded-lg p-4 mt-4 pr-9 shadow-md mr-4">
+          <div className="flex mr-3 w-9 h-9 border-2 border-blue-400 rounded-full bg-blue-600 text-white">
+            <ShoppingCartIcon className="h-6 w-6 mt-1 ml-1" />
+          </div>
+        </div>
+        <div className="w-1/4 h-40 bg-zinc-100 rounded-lg p-4 mt-4 pr-9 shadow-md mr-4">
+          <div className="flex mr-3 w-9 h-9 border-2 border-blue-400 rounded-full bg-blue-600 text-white">
+            <CheckIcon className="h-6 w-6 mt-1 ml-1" />
+          </div>
+        </div>
+        <div className="w-1/4 h-40 bg-zinc-100 rounded-lg p-4 mt-4 pr-9 shadow-md mr-4">
+          <div className="flex mr-3 w-9 h-9 border-2 border-blue-400 rounded-full bg-blue-600 text-white">
+            <DocumentCheckIcon className="h-6 w-6 mt-1 ml-1" />
+          </div>
+        </div>
+        <div className="w-1/4 h-40 bg-zinc-100 rounded-lg p-4 mt-4 pr-9 shadow-md">
+        <div className="flex mr-3 w-9 h-9 border-2 border-blue-400 rounded-full bg-blue-600 text-white">
+          <XMarkIcon className="h-6 w-6 mt-1 ml-1" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContentHome;
